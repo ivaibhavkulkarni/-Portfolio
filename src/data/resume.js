f1107fe15e42d8d3f29117b21aef24ce1671ee9f
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-11-21 11:00:36 +0530

    hactoberfinal

--- src/data/resume.js
+++ src/data/resume.js
@@ -45,7 +45,9 @@ export const achievements = [
     {
       title: "HacktoberFest 2024 Participation",
       points: [
-        " Demonstrated a strong commitment to open-source development by contributing to multiple MERN stack projects during Hacktoberfest. Successfully merged over four pull requests, showcasing effective team collaboration and adherence to best coding practices. Earned prestigious Hacktoberfest badges, reflecting dedication to continuous learning and active community involvement.",
+        " Demonstrated a strong commitment to open-source development by contributing to multiple MERN stack projects during Hacktoberfest.", 
+       "Successfully merged over four pull requests, showcasing effective team collaboration and adherence to best coding practices.",
+       "Earned prestigious Hacktoberfest badges, reflecting dedication to continuous learning and active community involvement.",
       ],
       image:"https://styles.redditmedia.com/t5_3gryx/styles/communityIcon_iegzxkwcwkmd1.png",
       link: "https://www.holopin.io/@ivaibhavkulkarni#"
